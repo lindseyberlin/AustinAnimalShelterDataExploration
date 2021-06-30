162e09c04c86b8977b8ec55ab3b54fec69a273da
lindseyberlin <linds.berlin@gmail.com>
2021-06-29 22:34:00 -0500

    Updated app code

--- VisualizingAnimalCenterData.py
+++ VisualizingAnimalCenterData.py
@@ -1,46 +1,72 @@
+# Imports
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from datetime import datetime, date
 
 st.title("Explore Austin's Animal Shelter Data")
 
-df = pd.read_csv("Intakes_2021_withlocationdetails.csv", parse_dates=['DateTime'])
+# Read in our data
+df = pd.read_csv("data/Austin_Animal_Center_Intakes_061521_with_location_details.csv", 
+                 parse_dates=['DateTime'])
 
-date_min, date_max = st.date_input(label="What time frame would you like to explore?", 
-                                   value=[date(2021, 1, 1), date(2021, 6, 9)], 
-                                   min_value=date(2021, 1, 1),
-                                   max_value=date(2021, 6, 9))
+# Adding date subset functionality in the sidebar
+# First getting the minimum and maximum dates out of our data
+date_min = df['DateTime'].min()
+date_max = df['DateTime'].max()
+# Then creating an input option
+range_min, range_max = st.sidebar.date_input(label="What time frame would you like to explore?", 
+                                             value=[date_min, date_max], 
+                                             min_value=date_min,
+                                             max_value=date_max)
+# Need to update the type to match with pandas
+range_min = pd.to_datetime(range_min)
+range_max = pd.to_datetime(range_max)                                     
+# Based on the above input, charts will only show details from that timeframe
+sub_df = df.loc[(df['DateTime'] <= range_max) &
+                (df['DateTime'] >= range_min)]
 
-date_min = pd.to_datetime(date_min)
-date_max = pd.to_datetime(date_max)
+# Adding an input option to the sidebar to toggle between visuals
+viz_option = st.sidebar.selectbox(label='Which data would you like to explore?',
+                                  options =['Types of Animal Intakes', 
+                                            'Animal Intakes Over Time', 
+                                            'Found Locations of Animals'])
 
-sub_df = df.loc[(df['DateTime'] <= date_max) &
-                (df['DateTime'] >= date_min)]
+# Visualizing our animal types
+if viz_option == 'Types of Animal Intakes':
+    st.header('Types of Animal Intakes')
+    # Building our types data
+    sub_df['Animal Type'] = sub_df['Animal Type'].replace({'Bird': 'Other', 'Livestock': 'Other'})
+    types = sub_df['Animal Type'].value_counts().reset_index()
+    types = types.rename(columns={'index':'Type', 'Animal Type': 'Count'})
+    # Visualizing our types data
+    fig1 = px.pie(types, values='Count', names='Type',
+                color_discrete_sequence=px.colors.qualitative.Pastel)
+    st.plotly_chart(fig1)
 
-sub_df = sub_df[~sub_df['found_lat'].isna()]
+# Visualizing our trends over time
+if viz_option == 'Animal Intakes Over Time':
+    st.header('Animal Intakes Over Time')
+    # Building our types data
+    sub_df['Year'] = sub_df['DateTime'].dt.year
+    sub_df['Month'] = sub_df['DateTime'].dt.month
+    annual_trend = sub_df.groupby(['Year', 'Month']).count()['Animal ID'].reset_index()
+    # Visualizing our trends data
+    fig2 = px.line(annual_trend, y='Animal ID', x='Month', color='Year',
+                labels={'Animal ID': 'Number of Animal Intakes'}) # better x label for clarity
+    st.plotly_chart(fig2)
 
-viz_df = sub_df.groupby(['Found Location', 'found_lat', 'found_lon']).count()[
-    'Animal ID'].reset_index()
-
-viz_df = viz_df.rename(columns={'Animal ID': 'count'})
-
-st.write("### Found Locations of Animals in that Time Frame:")
-
-fig = px.scatter_mapbox(
-    viz_df,
-    lat='found_lat',
-    lon='found_lon',
-    size="count",
-    color='count',
-    hover_name="Found Location",
-    zoom=9,
-)
-fig.update_layout(
-    mapbox_style="open-street-map",
-    margin={"r":0,"t":0,"l":0,"b":0},
-    mapbox=dict(
-        center={'lat': 30.2672, 'lon': -97.7431},
-    )
-)
-st.plotly_chart(fig)
+# Mapping our intakes
+if viz_option == 'Found Locations of Animals':
+    st.header('Found Locations of Animals')
+    # Building our location data
+    location_df = sub_df.loc[sub_df['Found Zipcode'].isna() == False]
+    location_count = location_df.groupby(by=['Found Location', 
+                                            'Found Latitude', 
+                                            'Found Longitude']).count()['Animal ID'].reset_index()
+    location_count = location_count.rename(columns={'Animal ID':'Count'})
+    # Visualizing our location data
+    fig3 = px.scatter_mapbox(location_count, lat="Found Latitude", lon="Found Longitude",
+                            color="Count", size="Count",zoom=10,
+                            hover_name='Found Location')
+    fig3.update_layout(mapbox_style="open-street-map")
+    st.plotly_chart(fig3)
